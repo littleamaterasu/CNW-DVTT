b41ad46270a3ea21a32516d1e4ebdcdb48f819fd
RouteHalo <tuyenthcsndusctn@gmail.com>
2024-05-22 00:17:36 +0700

    Add VNPAY payment

---
+++ src/main/java/com/cnweb36/API/VnPayAPI.java
@@ -0,0 +1,78 @@
+package com.cnweb36.API;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cnweb36.DTO.Entity.PaymentDTO;
+import com.cnweb36.DTO.Response.Book;
+import com.cnweb36.DTO.Response.NoticeResponse;
+import com.cnweb36.DTO.Response.PaymentResponse;
+import com.cnweb36.Entity.OrderEntity;
+import com.cnweb36.Entity.PaymentEntity;
+import com.cnweb36.Repository.AccountRepository;
+import com.cnweb36.Repository.PaymentRepository;
+import com.cnweb36.Service.PaymentService;
+import com.cnweb36.Service.Security.JwtUtility;
+import com.cnweb36.Service.VnPay.VnPayService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/payment")
+public class VnPayAPI {
+	
+	@Autowired
+	private JwtUtility jwtUtility;
+	@Autowired
+	private PaymentService paymentService;
+	@Autowired
+	private VnPayService vnpayService;
+	@Autowired
+	private AccountRepository accountRepository;
+	@Autowired
+	private PaymentRepository paymentRepository;
+	
+	@CrossOrigin(origins = "${cnweb36.crossOrigin}", allowCredentials = "true", maxAge = 3600)
+	@PreAuthorize("hasRole('ROLE_USER')")
+	@PostMapping("/vnpay/pay")
+	public String vnpayPayment(@CookieValue("${cnweb36.jwtCookieName}") String jwtToken, @RequestParam(name="paymentId") Long paymentId, 
+			HttpServletRequest request) {
+		String username = jwtUtility.getUserNameFromJwtToken(jwtToken);
+		PaymentResponse paymentResponse = paymentService.getPayment(username, paymentId);
+		if (paymentResponse != null && paymentResponse.getUserId() == accountRepository.findEntityByUsername(username).getId()) {
+	        String baseUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
+	        String vnpayUrl = vnpayService.createPayment(paymentResponse.getPay(), paymentId.toString(), baseUrl);
+	        return vnpayUrl;
+	        
+		} else {
+			return "";
+		}
+	}
+	
+	@CrossOrigin(origins = "${vnpay.crossOrigin}", maxAge = 3600)
+	@PostMapping("${vnpay.shortReturnUrl}")
+	public String vnpayPaymentReturn(HttpServletRequest request) {
+		int paymentStatus = vnpayService.paymentReturn(request);
+        Long paymentId = Long.valueOf(request.getParameter("vnp_OrderInfo"));
+//        String paymentTime = request.getParameter("vnp_PayDate");
+//        String transactionId = request.getParameter("vnp_TransactionNo");
+//        String totalPrice = request.getParameter("vnp_Amount");
+        
+		PaymentEntity paymentEntity = paymentRepository.findEntityById(paymentId);
+		paymentEntity.setStatus(paymentStatus == 1 ? "paymentSuccess" : "paymentFail");
+		paymentRepository.save(paymentEntity);
+        return paymentStatus == 1 ? "ordersuccess" : "orderfail";
+	}
+}
